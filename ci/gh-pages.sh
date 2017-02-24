079ac9e998fe10b25f3603f1c03effc391d58f9c
Dávid Halász <dhalasz@redhat.com>
2017-02-24 15:47:00 +0100

    Fixed Travis CI deployment to GitHub pages

--- ci/gh-pages.sh
+++ ci/gh-pages.sh
@@ -2,11 +2,14 @@
 set -x
 HASH=$(git rev-parse HEAD)
 mv -f assets tmp
-git checkout origin/gh-pages
+git remote add upstream https://github.com/ManageIQ/font-fabulous.git
+git fetch upstream
+git checkout upstream/gh-pages
 mv -f preview.html index.html
 rm -rf assets
 mv -f tmp assets
 git add index.html assets/fonts assets/stylesheets
-git commit -m 'GitHub pages build from ${HASH}'
-git push https://$GITHUB_AUTH@github.com/ManageIQ/font-fabulous HEAD:gh-pages
+git commit -m "GitHub pages build from ${HASH}"
 set +x
+echo "git push origin HEAD:gh-pages"
+git push https://$GITHUB_AUTH@github.com/ManageIQ/font-fabulous.git HEAD:gh-pages
