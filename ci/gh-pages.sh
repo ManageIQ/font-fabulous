295475e35db88eb9cd806e396f57e0da260fb770
Dávid Halász <dhalasz@redhat.com>
2017-02-27 18:57:46 +0100

    Revert back to deploy using https from ssh
    This reverts commit 481803376f4b7dae69559224906951724843bc37.
    This reverts commit 1a817bedb10bf787dbf2ee56453afa3d46a67e43.

--- ci/gh-pages.sh
+++ ci/gh-pages.sh
@@ -2,7 +2,7 @@
 set -x
 HASH=$(git rev-parse HEAD)
 mv -f assets tmp
-git remote add upstream git@github.com:ManageIQ/font-fabulous.git
+git remote add upstream https://github.com/ManageIQ/font-fabulous.git
 git fetch upstream
 git checkout upstream/gh-pages
 mv -f preview.html index.html
@@ -10,5 +10,6 @@ rm -rf assets
 mv -f tmp assets
 git add index.html assets/fonts assets/stylesheets
 git commit -m "GitHub pages build from ${HASH}"
-git push upstream HEAD:gh-pages
 set +x
+echo "git push origin HEAD:gh-pages"
+git push https://$GITHUB_AUTH@github.com/ManageIQ/font-fabulous.git HEAD:gh-pages &> /dev/null
